282e73a221fbf013d9be8777aaf0d1ff89049df9
Eden Kang <edenkang5517@gmail.com>
2023-01-29 02:49:46 +0900

    feat : init sequelize

---
+++ seeders/20230128172645-initHymns.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const hymnJSON = require('../hymn_list.json')
+    await queryInterface.bulkInsert('Hymns', hymnJSON)
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
