66a375a9ea884c65b5c6c7ec8647eb677ba3c71e
Eden Kang <edenkang5517@gmail.com>
2023-02-10 01:49:03 +0900

    feat : docker로 mysql과 연결

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create database
+npx sequelize db:create --env development
+
+# create table
+npx sequelize-cli db:migrate
+
+# insert initial data
+npx sequelize db:seed:all
+
+# start server
+npm start
